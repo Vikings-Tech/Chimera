1a33ed849dba17a3a6a0d1268302f9ab6e783fe5
MysteriousAcadia <anshumansingh64@gmail.com>
2020-08-11 15:37:01 +0530

    Add forest assets and procedural generation

---
+++ Assets/MainGame/Scripts/MapDisplay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+    
+   public void DrawNoiseMap(float[,] noiseMap)
+   {
+
+   }
+}
