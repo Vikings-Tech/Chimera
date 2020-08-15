07c165bfa6718b1373ba03f105ecf4fd6311d97c
MysteriousAcadia <anshumansingh64@gmail.com>
2020-08-16 03:57:49 +0530

    Add facebook authentication

---
+++ Assets/ProfileRenderer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class ProfileRenderer : MonoBehaviour
+{
+    public Image ProfilePic;
+    public Text Name;
+
+    public void SetProfilePic(Sprite _ProfilePic)
+    {
+        ProfilePic.sprite = _ProfilePic;
+    }
+
+    public void SetName(string _name)
+    {
+        Name.text = _name;
+    }
+}
